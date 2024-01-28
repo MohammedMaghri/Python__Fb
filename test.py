04b292e85b02507579e78b19bc9b603359289f22
Mohammed maghri <135870538+MohammedMaghri@users.noreply.github.com>
2024-01-28 22:46:39 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 import click
-# awkijitalj@gmail.com
-# amine2003
+# **********j@gmail.com
+# **********
 # Mohammed Amine Ait Bahcine
 string = input(" Please write The Gmail ....")
 sleep(2)
